5ec09b172ac58c0eaa5fb38d8d7bda3484b68ea0
mliuzzi69 <117594543+mliuzzi69@users.noreply.github.com>
2022-11-09 20:36:36 -0500

    Update ReviewClass.java

--- src/review/ReviewClass.java
+++ src/review/ReviewClass.java
@@ -4,8 +4,6 @@ public class ReviewClass {
 
 	public static void main(String[] args) {
 		System.out.println("this should be in Eclipse now");
-		
-		
 	}
 	
 	public void test() {
@@ -15,5 +13,8 @@ public class ReviewClass {
 	public void test2() {
 		System.out.println("This method is 2nd Review");
 	}
+	
+	public void test3(){
+		System.out.println("This method is 3rd review to pass to Eclipse");
 	
 }
